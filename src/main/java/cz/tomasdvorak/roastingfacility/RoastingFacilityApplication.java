b9d3b8385d634a144ae8275fb75c7c5ef85c2967
Tomas Dvorak <todvora@gmail.com>
2021-03-23 18:41:57 +0100

    initial project structure generated by https://start.spring.io/

---
+++ src/main/java/cz/tomasdvorak/roastingfacility/RoastingFacilityApplication.java
@@ -0,0 +1,13 @@
+package cz.tomasdvorak.roastingfacility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoastingFacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoastingFacilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/tomasdvorak/roastingfacility/RoastingFacilityApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tomasdvorak.roastingfacility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoastingFacilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
